224c7c3d5c7d6c3cd0d6d8f443f2e68a6a39bd6a
신주하 <ladea1224@icloud.com>
2026-08-23 22:37:07 +0900

    type: bfs

---
+++ codeforces/520B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,m;
+    cin >> n >> m;
+    if(n==m){
+        cout << 0;
+        return 0;
+    }
+
+    deque<int> deque = {n};
+    vector<int> dp(10010);
+
+    int x = 0;
+    while(!deque.empty()){
+        x = deque.front(); deque.pop_front();
+        if(x-1 >= 1 && !dp[x-1]){
+            dp[x-1] = dp[x] + 1;
+            if(x-1 == m) break;
+            deque.push_back(x-1);
+        }
+        if(x*2 <= m && !dp[x*2]){
+            dp[x*2] = dp[x] + 1;
+            if(x*2 == m) break;
+            deque.push_back(x*2);
+        }
+    }
+
+    cout << dp[m];
+
+    return 0;
+}
