1dc393c9a9096c6b66ccd87d2de370deb566cd9c
Ladea1224 <105615825+Ladea1224@users.noreply.github.com>
2026-09-21 14:04:10 +0900

    Update ［PCCE 기출문제］ 10번 ／ 공원.py

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -9,3 +9,21 @@ def solution(mats, park):
                     answer = max(answer,mat)
         
     return answer
+
+
+"""
+def solution(mats, park):
+    answer = -1
+    row,col = len(park),len(park[0]) #6,8
+    
+    for mat in mats:
+        if any(all(park[x+i][y+j]=="-1" 
+                    for i in range(mat) 
+                    for j in range(mat)
+                  ) for x in range(row-mat+1) 
+                    for y in range(col-mat+1)
+            ):
+            answer = max(answer,mat)
+        
+    return answer
+"""
