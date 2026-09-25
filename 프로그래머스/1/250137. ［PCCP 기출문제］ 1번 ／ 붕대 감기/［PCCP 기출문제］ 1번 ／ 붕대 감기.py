f09b9d632157a8413f96e77ead6d04e1212a19b0
Ladea1224 <105615825+Ladea1224@users.noreply.github.com>
2026-09-25 18:43:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,3 +1,4 @@
+"""
 def solution(bandage, health, attacks):
     maxH = health
     timeB,upB,bonusB = bandage[0],bandage[1],bandage[2]
@@ -16,3 +17,24 @@ def solution(bandage, health, attacks):
                 health = min(health+bonusB,maxH)
                 keep = 0
     return health
+"""
+#풀이2: 시간 간격 이용. 어차피 attack 사이에서 일어나는 일은 정해져 있으므로
+def solution(bandage, health, attacks):
+    maxH = health
+    timeB,upB,bonusB = bandage
+    prev = 0
+    
+    for timeA,damageA in attacks:
+        width = timeA-prev-1
+        if width>0:
+            health+= (width*upB) + (width//timeB)*bonusB
+            health = min(health,maxH)
+            
+        health -= damageA
+        if health<=0: return -1
+    
+        prev = timeA
+        
+    return health
+        
+    
