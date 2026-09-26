8d53b8ec6e6bdfdb718110669f3088b2c8362476
Ladea1224 <105615825+Ladea1224@users.noreply.github.com>
2026-09-26 11:16:31 +0900

    [level 2] Title: 과제 진행하기, Time: 1.02 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,7 +1,6 @@
 #반복문 내부에서의 순서 배치 혼동(경계 부분 주의) 
 #문제 조건 오해(23:59 까지 끝나는게 아니라 최대 시작 시간이 23:59)
-#예정: 시간 관련 문제 -> 일일이 초단위 진행하지말고 사건 사이의 시간 차 이용한 풀이 
-
+"""
 def convert(time):
     h,m = map(int,time.split(':'))
     return h*60 + m
@@ -34,7 +33,37 @@ def solution(plans):
         
     
     return [plans[idx][0] for idx in ended]
+"""
+#시간 관련 문제 -> 일일이 초단위 진행하지말고 사건 사이의 시간 차 이용한 풀이 
+#현재-과거 방식(이 풀이): 과거 저장 필요, 루프 전체 순회 가능
+#현재-미래 방식: 과거 저장 필요 X, 마지막 부분 루프 불가능, 예외처리 필요
+
+def convert(time):
+    h,m = map(int,time.split(':'))
+    return h*60 + m
+
+def solution(plans):
+    plans = [ [plan[0],convert(plan[1]),int(plan[2])] for plan in plans]
+    plans.sort(key=lambda x: x[1])
     
+    stack,answer=[],[]
+    prev = 0
+    for name,start,time in plans:
+        diff = start-prev
+        while stack and diff>0: #과거 처리 - stack 에 작업 있을 때, diff 갉아먹음
+            if stack[-1][1]<=diff:
+                diff -= stack[-1][1]
+                answer.append(stack[-1][0])
+                stack.pop()
+            else:
+                stack[-1][1] -= diff
+                diff = 0
+        stack.append([name,time]) #현재 처리 - 스택에 작업 추가 
+        prev = start
         
+    while stack:
+        answer.append(stack.pop()[0])
+        
+    return answer
         
     
